2c2136802e1d805acdc82d7b8c794a0520ca32e6
patilharish1211 <patilharish1211@gmail.com>
2024-09-21 10:13:45 +0530

    update

---
+++ API Hunter/src/App.jsx
@@ -0,0 +1,11 @@
+import AllRoutes from "./Routes/AllRoutes";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className="App">
+    
+      <AllRoutes />
+    </div>
+  );
+}
